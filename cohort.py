51dc68a7852d99ca4f1ac13b724eef0cada2fb5e
Rishi Vemireddy <vemireddyrishi@gmail.com>
2026-05-26 00:53:28 -0500

    Started Cluster Analysis

--- cohort.py
+++ cohort.py
@@ -35,3 +35,37 @@ plt.ylabel('Number of popular songs')
 plt.title('Identify the two albums with the most popular songs.')
 plt.tight_layout()
 plt.show()
+
+#Perform cluster analysis
+
+from sklearn.cluster import KMeans
+import numpy as np
+
+X = data[['energy', 'instrumentalness']]
+
+wcss = []
+
+for i in range(1,11):
+  model = KMeans(n_clusters=i, n_init=10, init='k-means++', random_state=42)
+  model.fit(X)
+  wcss.append(model.inertia_)
+plt.plot(range(1,11), wcss)
+plt.title('Finding optimal number of clusters')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+model = KMeans(n_clusters = 3, n_init = 10, init = 'k-means++', random_state=42)
+y_kmeans = model.fit_predict(X)
+
+X = X.values
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(model.cluster_centers_[:,0], model.cluster_centers_[:,1], s=300, c='yellow', label='Centroids')
+plt.title('Cohorts of songs')
+plt.xlabel('Energy')
+plt.ylabel('Instrumentalness')
+plt.legend()
+plt.show()
